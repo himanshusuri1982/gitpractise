8b31205a1ea2f417be36cd39b27ed3945f9b804b
HS <himanshu.suri2578@gmail.com>
2026-04-21 23:43:40 -0400

    a+b+c

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 print("hello world")
-def add(a, b):
-    return a + b
-print(add(2, 3))
+def add(a, b, C):
+    return a + b + C
+print(add(2, 3, 4))
 print("This is a test file in the main directory.")
 
 def subtract(a, b):
